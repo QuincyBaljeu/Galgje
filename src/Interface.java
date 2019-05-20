83e0f900eaaa43952bcc59b9849afe36e0366da8
StinovB <stijnberkel2906@gmail.com>
2019-05-20 15:55:00 +0200

    added images, made a new GUI

--- src/Interface.java
+++ src/Interface.java
@@ -1,17 +1,40 @@
+import com.sun.javafx.scene.control.skin.CustomColorDialog;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javafx.scene.image.ImageView;
+
+import javax.xml.soap.Text;
 
 public class Interface extends Application {
 
     public void start(Stage stage){
+        stage.setMinWidth(1000);
+        stage.setMinHeight(1000);
 
-        stage.setMaxHeight(700);
-        stage.setMaxWidth(700);
-        Button button = new Button("insert dooi manneke");
-        button.setMinSize(400, 400);
-        Scene scene = new Scene(button);
+        Image image = new Image("galgje11.png");
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(500);
+        imageView.setFitWidth(500);
+
+        Button button = new Button("guess letter");
+        TextField letterToGuess = new TextField();
+
+        HBox guessletterBox = new HBox();
+        guessletterBox.getChildren().addAll(letterToGuess, button);
+
+
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(imageView, guessletterBox);
+        Scene scene = new Scene(hBox);
         stage.setScene(scene);
         stage.show();
     }
