928f6e1179b21bf16633a55a76dc69fe2f25c9bb
Quincy Baljeu <q.baljeu@gmail,com>
2019-05-27 14:18:56 +0200

    Added image change on button press

--- src/Interface.java
+++ src/Interface.java
@@ -3,28 +3,47 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javafx.scene.image.ImageView;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Interface extends Application {
 
+    int wrongAnswers = 2;
+
+    public void wrong(){
+        wrongAnswers++;
+    }
+
+    @Override
     public void start(Stage stage){
         stage.setMinWidth(1000);
         stage.setMinHeight(1000);
 
-        Image image = new Image("rec/galgje2.png");
-        ImageView imageView = new ImageView(image);
 
-        imageView.setFitHeight(500);
-        imageView.setFitWidth(500);
+
+        Image image = new Image("file:C:/Users/qbalj/Documents/Galgje/res/galgje.png");
+        ImageView imageView = new ImageView();
+
+        imageView.setImage(image);
+        imageView.setFitWidth(100);
+
 
         Button button = new Button("guess letter");
+
+        button.setOnAction( event -> {
+            imageView.setImage(new Image("file:C:/Users/qbalj/Documents/Galgje/res/galgje" + wrongAnswers +".png"));
+            wrong();
+        });
+
+
         TextField letterToGuess = new TextField();
         TextField guessedLetters = new TextField();
-
 
         HBox guessletterBox = new HBox();
         guessletterBox.getChildren().addAll(letterToGuess, button);
@@ -32,10 +51,9 @@ public class Interface extends Application {
         VBox vBox = new VBox();
         vBox.getChildren().addAll(guessletterBox, guessedLetters);
 
-
-
         HBox hBox = new HBox();
         hBox.getChildren().addAll(imageView, vBox);
+
         Scene scene = new Scene(hBox);
         stage.setScene(scene);
         stage.show();
